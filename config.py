1681b961852b97a757ae4b35f03443d85f385c3c
Udeshika-SA <saudeshika21@gmail.com>
2026-07-05 15:59:06 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-SECRET_KEY = os.getenv("SECRET_KEY")
+DB_CONFIG = {
+    "host": "mysql-1e654038-spft-db.f.aivencloud.com",
+    "user": "avnadmin",
+    "port": "18680",
+    "password": "AVNS_vd3Q4-KwRwiSZxj82__",
+    "database": "defaultdb"
+}
