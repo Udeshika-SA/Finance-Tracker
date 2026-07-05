a0975b46a13c89c5462429f334a366fcbb2142ed
Udeshika-SA <saudeshika21@gmail.com>
2026-07-05 15:59:36 +0530

    Add files via upload

--- src/database/db.py
+++ src/database/db.py
@@ -1,10 +1,5 @@
 import mysql.connector
-from config import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
+from config import DB_CONFIG
 
-connection = mysql.connector.connect(
-    host=DB_HOST,
-    port=DB_PORT,
-    user=DB_USER,
-    password=DB_PASSWORD,
-    database=DB_NAME
-)
+def get_db():
+    return mysql.connector.connect(**DB_CONFIG)
